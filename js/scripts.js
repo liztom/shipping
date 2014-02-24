d02b2a260c979c9bc926e149753ad208f812581f
epicodus <student@epicodus.com>
2014-02-24 15:21:51 -0800

    calculate the cost of shipping

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+var Package = {
+  scale: function() {
+    if (this.weight > 0 && this.weight <= 2) {
+      return 3;
+  } else if (this.weight > 2 && this.weight <= 10) {
+      return 15;
+  } else if (this.weight > 10 && this.weight <= 50) {
+      return 25;
+  } else if (this.weight > 50 && this.weight <= 100) {
+     return 60;
+  } else {
+    return 'We only ship packages whose weight is less than 100 pounds';
+  }
+},
+  speed: function() {
+    if (this.shipTime === 'Priority') {
+      return this.scale(this.weight) + 10;
+    } else if (this.shipTime === 'Express') {
+      return this.scale(this.weight) + 30;
+    } else if (this.shipTime === 'Overnight'){
+      return this.scale(this.weight) + 50;
+    }
+  },
+  distance: function() {
+    var regions = parseInt(this.distanceShipped / 500);
+    var regionFee = regions * .25;
+    if (regions < 1) {
+       return this.speed();
+    } else {
+       return this.speed() + (this.speed()*regionFee);
+    }
+  }
+};
+
+$(document).ready(function() {
+  $('form#new-package').submit(function(event) {
+    event.preventDefault();
+
+    var inputWeight = parseInt($('input#weight').val());
+    var inputSpeed = $('input#speed').val();
+    var inputDistance = parseInt($('input#distance').val());
+
+    var newPackage = Object.create(Package);
+    newPackage.weight = inputWeight;
+    newPackage.speed = inputSpeed;
+    newPackage.distance = inputDistance;
+
+    $('.')
+  })
+}
+
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,26 @@
 describe("Package", function(){
-
+  describe("scale", function(){
+    it("Determines the base price to ship an object using weight", function(){
+      var testPackage = Object.create(Package);
+      testPackage.weight = 12;
+      testPackage.scale().should.equal(25);
+    });
+  });
+  describe('speed', function() {
+    it('adds 10 for priority, 30 for express, and 50 for overnight', function() {
+      var testPackage = Object.create(Package);
+      testPackage.weight = 12;
+      testPackage.shipTime = 'Express';
+      testPackage.speed().should.equal(55);
+    });
+  });
+  describe('distance', function(){
+    it('increases the price by 25% for every 500 miles past the first 500', function(){
+      var testPackage = Object.create(Package);
+      testPackage.weight = 12;
+      testPackage.shipTime = 'Express';
+      testPackage.distanceShipped = 1600;
+      testPackage.distance().should.equal(96.25)
+    });
+  });
 });
